bb587a77d8bce92242ea57fda23dc94562e48bb7
ehco1996 <zh19960202@gmail.com>
2019-09-11 13:53:25 +0800

    update

---
+++ golang/easy/520.检测大写字母.go
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=520 lang=golang
+ *
+ * [520] 检测大写字母
+ *
+ * https://leetcode-cn.com/problems/detect-capital/description/
+ *
+ * algorithms
+ * Easy (53.35%)
+ * Likes:    64
+ * Dislikes: 0
+ * Total Accepted:    10.4K
+ * Total Submissions: 19.4K
+ * Testcase Example:  '"USA"'
+ *
+ * 给定一个单词，你需要判断单词的大写使用是否正确。
+ *
+ * 我们定义，在以下情况时，单词的大写用法是正确的：
+ *
+ *
+ * 全部字母都是大写，比如"USA"。
+ * 单词中所有字母都不是大写，比如"leetcode"。
+ * 如果单词不只含有一个字母，只有首字母大写， 比如 "Google"。
+ *
+ *
+ * 否则，我们定义这个单词没有正确使用大写字母。
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: "USA"
+ * 输出: True
+ *
+ *
+ * 示例 2:
+ *
+ *
+ * 输入: "FlaG"
+ * 输出: False
+ *
+ *
+ * 注意: 输入是由大写和小写拉丁字母组成的非空单词。
+ *
+ */
+import "strings"
+
+func detectCapitalUse(word string) bool {
+	upCnt := 0
+	firstIsUpper := false
+
+	for idx, w := range word {
+		w := string(w)
+		if strings.ToUpper(w) == w {
+			if idx == 0 {
+				firstIsUpper = true
+			}
+			upCnt += 1
+		}
+	}
+	return len(word) == upCnt || upCnt == 0 || (upCnt == 1 && firstIsUpper)
+
+}
+
---
+++ golang/easy/521.最长特殊序列-ⅰ.go
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=521 lang=golang
+ *
+ * [521] 最长特殊序列 Ⅰ
+ *
+ * https://leetcode-cn.com/problems/longest-uncommon-subsequence-i/description/
+ *
+ * algorithms
+ * Easy (63.12%)
+ * Likes:    35
+ * Dislikes: 0
+ * Total Accepted:    5.8K
+ * Total Submissions: 9.2K
+ * Testcase Example:  '"aba"\n"cdc"'
+ *
+ * 给定两个字符串，你需要从这两个字符串中找出最长的特殊序列。最长特殊序列定义如下：该序列为某字符串独有的最长子序列（即不能是其他字符串的子序列）。
+ *
+ * 子序列可以通过删去字符串中的某些字符实现，但不能改变剩余字符的相对顺序。空序列为所有字符串的子序列，任何字符串为其自身的子序列。
+ *
+ * 输入为两个字符串，输出最长特殊序列的长度。如果不存在，则返回 -1。
+ *
+ * 示例 :
+ *
+ * 输入: "aba", "cdc"
+ * 输出: 3
+ * 解析: 最长特殊序列可为 "aba" (或 "cdc")
+ *
+ *
+ * 说明:
+ *
+ *
+ * 两个字符串长度均小于100。
+ * 字符串中的字符仅含有 'a'~'z'。
+ *
+ *
+ */
+func findLUSlength(a string, b string) int {
+	//如果两个字符串长度不一样，则较长的字符串本身不可能是短字符串的子序列，直接返回其长度即可
+	//如果两个字符串内容相等，那么他们独有的最长子序列不存在，返回 -1
+	if a == b {
+		return -1
+	} else {
+		if len(a) > len(b) {
+			return len(a)
+		} else {
+			return len(b)
+		}
+	}
+
+}
+
