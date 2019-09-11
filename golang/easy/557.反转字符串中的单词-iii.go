f4178f6b396f20b2239f3f8e0e171c7dea2315d7
ehco1996 <zh19960202@gmail.com>
2019-09-11 20:50:23 +0800

    update

---
+++ golang/easy/557.反转字符串中的单词-iii.go
@@ -0,0 +1,52 @@
+// package easy
+
+/*
+ * @lc app=leetcode.cn id=557 lang=golang
+ *
+ * [557] 反转字符串中的单词 III
+ *
+ * https://leetcode-cn.com/problems/reverse-words-in-a-string-iii/description/
+ *
+ * algorithms
+ * Easy (66.79%)
+ * Likes:    119
+ * Dislikes: 0
+ * Total Accepted:    27.4K
+ * Total Submissions: 40.6K
+ * Testcase Example:  `"Let's take LeetCode contest"`
+ *
+ * 给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: "Let's take LeetCode contest"
+ * 输出: "s'teL ekat edoCteeL tsetnoc"
+ *
+ *
+ * 注意：在字符串中，每个单词由单个空格分隔，并且字符串中不会有任何额外的空格。
+ *
+ */
+
+func reverse(b []byte) {
+	left, right := 0, len(b)-1
+	for left < right {
+		b[left], b[right] = b[right], b[left]
+		left, right = left+1, right-1
+	}
+}
+
+func reverseWords(s string) string {
+	sb := []byte(s)
+
+	last := 0
+	for i := 0; i < len(sb); i++ {
+		if string(sb[i]) == " " {
+			reverse(sb[last:i])
+			last = i + 1
+		} else if i == len(sb)-1 {
+			reverse(sb[last:])
+		}
+	}
+	return string(sb)
+}
---
+++ golang/easy/561.数组拆分-i.go
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=561 lang=golang
+ *
+ * [561] 数组拆分 I
+ *
+ * https://leetcode-cn.com/problems/array-partition-i/description/
+ *
+ * algorithms
+ * Easy (66.67%)
+ * Likes:    115
+ * Dislikes: 0
+ * Total Accepted:    19.1K
+ * Total Submissions: 28.4K
+ * Testcase Example:  '[1,4,3,2]'
+ *
+ * 给定长度为 2n 的数组, 你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从1 到
+ * n 的 min(ai, bi) 总和最大。
+ *
+ * 示例 1:
+ *
+ *
+ * 输入: [1,4,3,2]
+ *
+ * 输出: 4
+ * 解释: n 等于 2, 最大总和为 4 = min(1, 2) + min(3, 4).
+ *
+ *
+ * 提示:
+ *
+ *
+ * n 是正整数,范围在 [1, 10000].
+ * 数组中的元素范围在 [-10000, 10000].
+ *
+ *
+ */
+import "sort"
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func arrayPairSum(nums []int) int {
+	sort.Ints(nums)
+	sum := 0
+	for i := 0; i < len(nums); i += 2 {
+		sum += min(nums[i], nums[i+1])
+	}
+	return sum
+}
+
