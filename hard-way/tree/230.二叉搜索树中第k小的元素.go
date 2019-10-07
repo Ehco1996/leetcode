cd54b9b2c7f3dde90a09677fcd3fb5c6a0b088f4
ehco1996 <zh19960202@gmail.com>
2019-10-07 08:10:14 +0800

    tree 2/4

---
+++ hard-way/tree/230.二叉搜索树中第k小的元素.go
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode.cn id=230 lang=golang
+ *
+ * [230] 二叉搜索树中第K小的元素
+ *
+ * https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/description/
+ *
+ * algorithms
+ * Medium (65.63%)
+ * Likes:    105
+ * Dislikes: 0
+ * Total Accepted:    23.1K
+ * Total Submissions: 34.9K
+ * Testcase Example:  '[3,1,4,null,2]\n1'
+ *
+ * 给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
+ *
+ * 说明：
+ * 你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。
+ *
+ * 示例 1:
+ *
+ * 输入: root = [3,1,4,null,2], k = 1
+ * ⁠  3
+ * ⁠ / \
+ * ⁠1   4
+ * ⁠ \
+ * 2
+ * 输出: 1
+ *
+ * 示例 2:
+ *
+ * 输入: root = [5,3,6,2,4,null,null,1], k = 3
+ * ⁠      5
+ * ⁠     / \
+ * ⁠    3   6
+ * ⁠   / \
+ * ⁠  2   4
+ * ⁠ /
+ * ⁠1
+ * 输出: 3
+ *
+ * 进阶：
+ * 如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化 kthSmallest 函数？
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func kthSmallest(root *TreeNode, k int) int {
+	// preorder
+	res := 0
+	cnt := 0
+	var helper func(node *TreeNode)
+	helper = func(node *TreeNode) {
+		if node.Left != nil {
+			helper(node.Left)
+		}
+		cnt++
+		if cnt == k {
+			res = node.Val
+		}
+		if node.Right != nil {
+			helper(node.Right)
+		}
+	}
+
+	helper(root)
+	return res
+}
+
+// @lc code=end
+
---
+++ hard-way/tree/236.二叉树的最近公共祖先.go
@@ -0,0 +1,94 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=golang
+ *
+ * [236] 二叉树的最近公共祖先
+ *
+ * https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+ *
+ * algorithms
+ * Medium (56.36%)
+ * Likes:    215
+ * Dislikes: 0
+ * Total Accepted:    20.5K
+ * Total Submissions: 36.4K
+ * Testcase Example:  '[3,5,1,6,2,0,8,null,null,7,4]\n5\n1'
+ *
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ *
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x
+ * 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ *
+ * 例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+ *
+ *
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * 输出: 3
+ * 解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+ *
+ *
+ * 示例 2:
+ *
+ * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+ * 输出: 5
+ * 解释: 节点 5 和节点 4 的最近公共祖先是节点 5。因为根据定义最近公共祖先节点可以为节点本身。
+ *
+ *
+ *
+ *
+ * 说明:
+ *
+ *
+ * 所有节点的值都是唯一的。
+ * p、q 为不同节点且均存在于给定的二叉树中。
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for TreeNode.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *ListNode
+ *     Right *ListNode
+ * }
+ */
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	// 层序遍历 不停的向下找
+	ans := root
+
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		node := queue[0]
+		// dequeue
+		queue = queue[1:]
+		ans = node
+
+		if node.Left != nil &&
+			findNode(q.Val, node.Left) && findNode(p.Val, node.Left) {
+			queue = append(queue, node.Left)
+		}
+		if node.Right != nil &&
+			findNode(q.Val, node.Right) && findNode(p.Val, node.Right) {
+			queue = append(queue, node.Right)
+		}
+	}
+	return ans
+}
+
+func findNode(val int, node *TreeNode) bool {
+	if node == nil {
+		return false
+	} else if node.Val == val {
+		return true
+	}
+	return findNode(val, node.Left) || findNode(val, node.Right)
+}
+
+// @lc code=end
+
