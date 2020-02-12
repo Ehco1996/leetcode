7b3598cb5c0b22e7a21fb8e4a5994a3adf0ba17b
Ehco1996 <zh19960202@gmail.com>
2020-02-12 08:46:28 +0800

    add dlc: bytedance linkedlist

---
+++ dlc/bytedance/linkedlist&&tree/2.两数相加.go
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=golang
+ *
+ * [2] 两数相加
+ *
+ * https://leetcode-cn.com/problems/add-two-numbers/description/
+ *
+ * algorithms
+ * Medium (34.32%)
+ * Likes:    2576
+ * Dislikes: 0
+ * Total Accepted:    146.6K
+ * Total Submissions: 424.3K
+ * Testcase Example:  '[2,4,3]\n[5,6,4]'
+ *
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ *
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ *
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ *
+ * 示例：
+ *
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ *
+ *
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	// carry 表示十分有进一位
+	ret := &ListNode{Val: 0}
+	cur := ret
+	carry := 0
+	for l1 != nil || l2 != nil {
+		sum := 0
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+		// 看是否要进位
+		if carry == 1 {
+			sum += 1
+			carry = 0
+		}
+		if sum > 9 {
+			sum -= 10
+			carry = 1
+		}
+
+		cur.Next = &ListNode{Val: sum}
+		cur = cur.Next
+		if l1 == nil && l2 == nil && carry == 1 {
+			cur.Next = &ListNode{Val: 1}
+		}
+	}
+
+	if ret.Next != nil {
+		return ret.Next
+	}
+	return ret
+}
---
+++ dlc/bytedance/linkedlist&&tree/206.反转链表.go
@@ -0,0 +1,73 @@
+package easy
+
+/*
+ * @lc app=leetcode.cn id=206 lang=golang
+ *
+ * [206] 反转链表
+ *
+ * https://leetcode-cn.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (63.37%)
+ * Likes:    553
+ * Dislikes: 0
+ * Total Accepted:    92.2K
+ * Total Submissions: 143.7K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 反转一个单链表。
+ *
+ * 示例:
+ *
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ *
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ *
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+// type ListNode struct {
+// 	Val  int
+// 	Next *ListNode
+// }
+
+func reverseList(head *ListNode) *ListNode {
+	// 不断将节点移动到链表头部
+
+	if head == nil {
+		return head
+	}
+
+	dummy := head
+	for head.Next != nil {
+		node := head.Next
+		head.Next = node.Next
+
+		node.Next = dummy
+		dummy = node
+
+	}
+	return dummy
+}
+
+func reverseList(head *ListNode) *ListNode {
+	// 递归版
+	// https://zhuanlan.zhihu.com/p/86745433
+
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	last := reverseList(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return last
+}
