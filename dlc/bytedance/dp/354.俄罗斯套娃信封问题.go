72251ba82aeba648a9a49d0eb4bcccd375ca291a
Ehco1996 <zh19960202@gmail.com>
2020-02-20 08:27:42 +0800

    update dlc:  bytedance dp

--- dlc/bytedance/dp/354.俄罗斯套娃信封问题.go
+++ dlc/bytedance/dp/354.俄罗斯套娃信封问题.go
@@ -31,16 +31,45 @@
  */
 
 // @lc code=start
+import "sort"
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
 func maxEnvelopes(envelopes [][]int) int {
-	// dp先将输入数组按wh排序，保证能包含当前信封的信封一定在当前信封之后，
+	// dp dp(j) 表示当前信封能包含的最大的信封数量
+	// 先将输入数组按wh排序，保证能包含当前信封的信封一定在当前信封之后，
 	// 然后从前往后更新dp，如果遇到能包含当前信封i的信封j，
 	// 则dp[j]=max(dp[j],dp[i]+1)
 
 	if len(envelopes) == 0 {
 		return 0
 	}
-	// TODO
 
+	// sort by interval[0]
+	sort.SliceStable(envelopes, func(i, j int) bool {
+		return envelopes[i][0] < envelopes[j][0]
+	})
+
+	dp := make([]int, len(envelopes))
+	for i := 0; i < len(envelopes); i++ {
+		for j := i; j < len(envelopes); j++ {
+			if envelopes[j][0] > envelopes[i][0] && envelopes[j][1] > envelopes[i][1] {
+				dp[j] = max(dp[j], dp[i]+1)
+			}
+		}
+	}
+
+	ans := dp[0]
+	for _, v := range dp {
+		ans = max(v, ans)
+	}
+	// +1 是因为自己也是一个信封
+	return ans + 1
 }
 
 // @lc code=end
