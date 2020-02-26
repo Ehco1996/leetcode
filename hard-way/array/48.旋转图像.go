78d68f1f041f35f3249b1b3808db0dd1f36d05e9
Ehco1996 <zh19960202@gmail.com>
2020-02-27 07:58:56 +0800

    update array

---
+++ hard-way/array/48.旋转图像.go
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode.cn id=48 lang=golang
+ *
+ * [48] 旋转图像
+ *
+ * https://leetcode-cn.com/problems/rotate-image/description/
+ *
+ * algorithms
+ * Medium (63.13%)
+ * Likes:    283
+ * Dislikes: 0
+ * Total Accepted:    36K
+ * Total Submissions: 55.7K
+ * Testcase Example:  '[[1,2,3],[4,5,6],[7,8,9]]'
+ *
+ * 给定一个 n × n 的二维矩阵表示一个图像。
+ *
+ * 将图像顺时针旋转 90 度。
+ *
+ * 说明：
+ *
+ * 你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+ *
+ * 示例 1:
+ *
+ * 给定 matrix =
+ * [
+ * ⁠ [1,2,3],
+ * ⁠ [4,5,6],
+ * ⁠ [7,8,9]
+ * ],
+ *
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ * ⁠ [7,4,1],
+ * ⁠ [8,5,2],
+ * ⁠ [9,6,3]
+ * ]
+ *
+ *
+ * 示例 2:
+ *
+ * 给定 matrix =
+ * [
+ * ⁠ [ 5, 1, 9,11],
+ * ⁠ [ 2, 4, 8,10],
+ * ⁠ [13, 3, 6, 7],
+ * ⁠ [15,14,12,16]
+ * ],
+ *
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ * ⁠ [15,13, 2, 5],
+ * ⁠ [14, 3, 4, 1],
+ * ⁠ [12, 6, 8, 9],
+ * ⁠ [16, 7,10,11]
+ * ]
+ *
+ *
+ */
+
+func reverse(nums []int) {
+	// 翻转
+	for l, r := 0, len(nums)-1; l < r; {
+		nums[l], nums[r] = nums[r], nums[l]
+		l++
+		r--
+	}
+}
+
+func transpose(matrix [][]int) {
+	// 转置 A[i,j] -> A[j,i]
+	for i := 0; i < len(matrix); i++ {
+		for j := i; j < len(matrix); j++ {
+			matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+		}
+	}
+}
+
+func rotate(matrix [][]int) {
+	// 顺时针旋转
+	// 先转置 再翻转每一行
+	transpose(matrix)
+	for i := 0; i < len(matrix); i++ {
+		reverse(matrix[i])
+	}
+}
