ee6d78ddfaf247e176cb443e198aed90e4be349d
Ehco1996 <zh19960202@gmail.com>
2020-12-18 09:15:29 +0800

    567: 字符串的排列

--- labuladong/开始之前/swindow/76.最小覆盖子串.py
+++ labuladong/开始之前/swindow/76.最小覆盖子串.py
@@ -5,16 +5,16 @@
 #
 
 # @lc code=start
+from collections import defaultdict
+
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
 
-        window = dict()
-        need = dict()
+        window = defaultdict(int)
+        need = defaultdict(int)
         for c in t:
-            if c not in need:
-                need[c] = 1
-            else:
-                need[c] += 1
+            need[c] += 1
 
         valid = 0  # 表示窗口中满足need条件的字符串的字符个数
         left, right = 0, 0
@@ -28,15 +28,12 @@ class Solution:
             right += 1
             if c in need:
                 # 更新窗口里的数
-                if c in window:
-                    window[c] += 1
-                else:
-                    window[c] = 1
+                window[c] += 1
                 if need[c] == window[c]:
                     valid += 1
 
             # 判断左侧是不是要收缩了（当前是否已经满足条件了）
-            while valid == len(t):
+            while valid == len(need):
                 # 更新最小子串
                 if (right - left) < length:
                     start = left
@@ -46,9 +43,9 @@ class Solution:
                 left += 1
                 # 将最左侧的数据从window中移除
                 if d in need:
-                    window[d] -= 1
                     if need[d] == window[d]:
                         valid -= 1
+                    window[d] -= 1
         if length != len(s) + 1:
             return s[start : start + length]
         return ""
